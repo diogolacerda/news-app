6c26b5f795d810c38d93a55dd9592c14b9f88437
Diogo Lacerda <diogo.lacerda@gmail.com>
2024-11-09 20:14:57 -0300

    feat: css fixes

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -23,13 +23,13 @@
 
             @if(request()->routeIs('home.index'))
                 <div class="hidden sm:flex sm:items-center sm:ms-6">
-                    <form action="{{ route('home.index') }}" method="GET" class="mt-4">
-                        <div class="form-control">
-                            <input type="text" name="search" placeholder="Buscar notícias..." value="{{ request('search') }}" class="input input-bordered input-sm w-24 md:w-auto" />
-                        </div>
+                    <form action="{{ route('home.index') }}" method="GET" class="flex items-center ">
+                        <input type="text" name="search" placeholder="Buscar notícias..." value="{{ request('search') }}" class="input input-bordered input-sm w-24 md:w-auto" />
+                        <button type="submit" class="btn btn-sm ml-2">Buscar</button>
                     </form>
                 </div>
             @endif
+
 
             <!-- Hamburger -->
             <div class="-me-2 flex items-center sm:hidden">
@@ -58,11 +58,14 @@
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200">
-            <div class="px-4">
-                <div class="form-control">
-                    <input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
-                </div>
+            @if(request()->routeIs('home.index'))
+            <div class="block sm:hidden px-4 py-2">
+                <form action="{{ route('home.index') }}" method="GET" class="flex items-center space-x-2">
+                    <input type="text" name="search" placeholder="Buscar notícias..." value="{{ request('search') }}" class="input input-bordered input-sm w-full" />
+                    <button type="submit" class="btn btn-xs btn-primary">Buscar</button>
+                </form>
             </div>
+        @endif
         </div>
     </div>
 </nav>
